fbeae9c4107e307faf5b8ba937eba81278af87f5
Brock Wilcox <awwaiid@thelackthereof.org>
2021-12-07 21:16:10 -0500

    Update watcher to be almost parameterized

--- util/sync.sh
+++ util/sync.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 
-rsync -avzP . --exclude .git --exclude ui --delete we@norns.local:/home/we/dust/code/repl-looper
+# Filename as first argument is optional
+DIRECTORY=${1:-/home/we/dust/code/repl-looper}
+FILENAME=${2:-repl-looper.lua}
 
-# Reload code
+# Sync the whole project directory
+rsync -avzP . --exclude .git --exclude ui --delete we@norns.local:$DIRECTORY
 
+# Reload "current"
 # echo 'norns.script.reload()' | \
 #   websocat ws://norns.local:5555 --protocol bus.sp.nanomsg.org -1
 
+# Reload our script specifically
 echo 'norns.script.load("code/repl-looper/repl-looper.lua")' | \
   websocat ws://norns.local:5555 --protocol bus.sp.nanomsg.org -1
+
+# Reload, but without restart
+# echo "dofile('$DIRECTORY/$FILENAME')" | \
+#   websocat ws://norns.local:5555 --protocol bus.sp.nanomsg.org -1
 
--- util/watch-sync.sh
+++ util/watch-sync.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # One time send
-echo 'norns.script.load("code/repl-looper/repl-looper.lua")' | \
-  websocat ws://norns.local:5555 --protocol bus.sp.nanomsg.org -1
+# echo 'norns.script.load("code/repl-looper/repl-looper.lua")' | \
+#   websocat ws://norns.local:5555 --protocol bus.sp.nanomsg.org -1
 
 # Then reload when the file changes
 nodemon --exec ./util/sync.sh -w repl-looper.lua
