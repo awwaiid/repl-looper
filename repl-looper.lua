d7dc2c2cb54946c60dc6e4a3036a3139264626ae
Brock Wilcox <awwaiid@thelackthereof.org>
2021-12-24 12:19:48 -0500

    Add molly synth wrapper

--- repl-looper.lua
+++ repl-looper.lua
@@ -936,10 +936,93 @@ function Sample:note(note)
 end
 
 
-molly = {}
+Molly = {}
+Molly.__index = Molly
+Molly.next_id = 0
 
-function molly.p(note, voice_id, sample_id)
-  local voice_id = voice_id or 0
+function Molly.new(filename, play_mode)
+  local self = {
+    params = {}
+  }
+  setmetatable(self, Molly)
+
+  self.id = Molly.next_id
+  Molly.next_id = Molly.next_id + 1
+
+  return self
+end
+
+local mollyVoiceFunctions = {
+  noteOn = "mollyNoteOn",
+  noteOff = "mollyNoteOff",
+  pitchBend = "mollyPitchBend",
+  noteKill = "mollyNoteKill",
+  pressure = "mollyPressure",
+  timbre = "mollyTimbre"
+}
+
+for funcName, engineFuncName in pairs(mollyVoiceFunctions) do
+  Molly[funcName] = function(self, ...)
+    engine[engineFuncName](self.id, ...)
+  end
+end
+
+local mollyFunctions = {
+  noteOffAll = "mollyNoteOffAll",
+  noteKillAll = "mollyNoteKillAll",
+  pitchBendAll = "mollyPitchBendAll",
+  pressureAll = "mollyPressureAll",
+  timbreAll = "mollyTimbreAll",
+  oscWaveShape = "mollyOscWaveShape",
+  pwMod = "mollyPwMod",
+  pwModSource = "mollyPwModSource",
+  freqModLfo = "mollyFreqModLfo",
+  freqModEnv = "mollyFreqModEnv",
+  glide = "mollyGlide",
+  mainOscLevel = "mollyMainOscLevel",
+  subOscLevel = "mollySubOscLevel",
+  subOscDetune = "mollySubOscDetune",
+  noiseLevel = "mollyNoiseLevel",
+  hpFilterCutoff = "mollyHpFilterCutoff",
+  lpFilterType = "mollyLpFilterType",
+  lpFilterCutoff = "mollyLpFilterCutoff",
+  lpFilterResonance = "mollyLpFilterResonance",
+  lpFilterCutoffEnvSelect = "mollyLpFilterCutoffEnvSelect",
+  lpFilterCutoffModEnv = "mollyLpFilterCutoffModEnv",
+  lpFilterCutoffModLfo = "mollyLpFilterCutoffModLfo",
+  lpFilterTracking = "mollyLpFilterTracking",
+  lfoFade = "mollyLfoFade",
+  env1Attack = "mollyEnv1Attack",
+  env1Decay = "mollyEnv1Decay",
+  env1Sustain = "mollyEnv1Sustain",
+  env1Release = "mollyEnv1Release",
+  env2Attack = "mollyEnv2Attack",
+  env2Decay = "mollyEnv2Decay",
+  env2Sustain = "mollyEnv2Sustain",
+  env2Release = "mollyEnv2Release",
+  ampMod = "mollyAmpMod",
+  ringModFade = "mollyRingModFade",
+  ringModMix = "mollyRingModMix",
+  amp = "mollyAmp",
+  chorusMix = "mollyChorusMix",
+  lfoFreq = "mollyLfoFreq",
+  lfoWaveShape = "mollyLfoWaveShape",
+  ringModFreq = "mollyRingModFreq"
+}
+
+for funcName, engineFuncName in pairs(mollyFunctions) do
+  Molly[funcName] = function(self, ...)
+    engine[engineFuncName](...)
+  end
+end
+
+function Molly:randomize_params(sound_type)
+  sound_type = sound_type or "lead"
+  MollyThePoly.randomize_params(sound_type)
+end
+
+function Molly:note(note)
+  local voice_id = self.id
   local note = note or 60
   local freq = 0
 
@@ -964,6 +1047,11 @@ function molly.p(note, voice_id, sample_id)
   engine.mollyNoteOn(voice_id, freq, 1)
 end
 
+function Molly:play() self:note() end
+function Molly:stop() self:noteOff() end
+
+-- One global molly to start with
+molly = Molly.new()
 
 ---------------------------------------------------------------------
 -- Load up and mess with some samples for performance ---------------
