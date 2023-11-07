e0dc4e8416fb630e3f5c568d6751e1d6bd4b3d33
Brock Wilcox <awwaiid@thelackthereof.org>
2023-11-06 20:59:58 -0500

    Add Grid and Editor classes

--- repl-looper.lua
+++ repl-looper.lua
@@ -49,6 +49,198 @@ function bug(...)
 end
 
 ---------------------------------------------------------------------
+-- Grid Wrapper -----------------------------------------------------
+---------------------------------------------------------------------
+
+local Grid = {}
+Grid.__index = Grid
+
+function Grid.new(key_handler)
+  local self = {
+    device = grid.connect(),
+    key = key_handler,
+    data = {}
+  }
+
+  -- Initialize data based on grid cols/rows
+  for x = 1, 16 do
+    self.data[x] = {}
+    for y = 1, 8 do
+      self.data[x][y] = 0
+    end
+  end
+
+  setmetatable(self, Grid)
+  return self
+end
+
+function Grid:refresh()
+  self.device:refresh()
+  redraw()
+end
+
+function Grid:led(x, y, brightness)
+  self.device:led(x, y, brightness)
+  self.data[x] = self.data[x] or {}
+  self.data[x][y] = brightness
+  -- redraw()
+end
+
+function Grid:all(brightness)
+  self.device:all(brightness)
+  self.data = {}
+  for x = 1, 16 do
+    self.data[x] = {}
+    for y = 1, 8 do
+      self.data[x][y] = 0
+    end
+  end
+  -- redraw()
+end
+
+---------------------------------------------------------------------
+-- Editor -----------------------------------------------------------
+---------------------------------------------------------------------
+
+local Editor = {}
+Editor.__index = Editor
+
+function Editor.new()
+  local self = {
+    content = "",
+    draw_at_x = 0,
+    draw_at_y = 62,
+    cursor = 1
+  }
+  setmetatable(self, Editor)
+  return self
+end
+
+function Editor:redraw()
+  -- This allows us to have a bottom-aligned editor
+  local height = self:draw_wrapped_content(0, 0, false)
+  self:draw_wrapped_content(0, self.draw_at_y - height, true)
+  return height
+end
+
+function Editor:draw_wrapped_content(start_x, start_y, do_draw)
+  -- Draw characters one at a time based on width
+  -- And then reverse the currect character if it is where the cursor is
+  -- screen.text draws from the lower-left corner for some weird reason, that's
+  -- weird. I guess we'll call this method twice, first to get the height and the second to draw the content
+  local content = self.content .. " " -- Add a space so we can see the cursor
+  local x = start_x
+  local y = start_y
+  for i = 1, #content do
+    local char = content:sub(i, i)
+    local char_width = screen.text_extents(char)
+    if char_width == 0 then
+      char_width = 4
+    end
+    if x + char_width > 127 then
+      x = 0
+      y = y + 8
+    end
+    if do_draw then
+      screen.move(x, y)
+      if i == self.cursor then
+        screen.level(15)
+        screen.text(char)
+        self:invert_rect(x, y-7, char_width, 9)
+      else
+        screen.level(15)
+        -- print("Drawing", char, "at", x, y)
+        screen.text(char)
+      end
+    end
+    -- screen.text(char)
+    x = x + char_width + 1
+  end
+  return y
+end
+
+function Editor:invert_rect(x, y, width, height)
+  -- print("Inverting rect", x, y, width, height)
+  local rect = screen.peek(x, y, width, height)
+  local out = {}
+  for i = 1, #rect do
+    out[i] = string.char(15 - string.byte(rect, i))
+    -- print("Inverting", i, string.byte(rect, i), out[i])
+  end
+  screen.poke(x, y, width, height, table.concat(out))
+end
+
+function Editor:wrap_text(text, max_len)
+  local lines = {}
+  local remaining_text = text
+  local current_line = ""
+  local character_number = 1
+  while #remaining_text > 0 do
+    local next_character = remaining_text:sub(1, 1)
+    local current_line_size = screen.text_extents(current_line .. next_character)
+    print("current_line_size", current_line_size)
+    if current_line_size > max_len then
+      table.insert(lines, current_line)
+      current_line = ""
+    end
+    current_line = current_line .. next_character
+    remaining_text = remaining_text:sub(2)
+    character_number = character_number + 1
+  end
+  table.insert(lines, current_line)
+  print("unwrapped:", text, "wrapped:", table.concat(lines, "\n"))
+  return lines
+  -- return table.concat(lines, "\n")
+end
+
+function Editor:insert(char)
+  self.content = self.content:sub(1, self.cursor - 1) .. char .. self.content:sub(self.cursor)
+  self.cursor = self.cursor + 1
+end
+
+function Editor:backspace()
+  if self.cursor > 1 then
+    self.content = self.content:sub(1, self.cursor - 2) .. self.content:sub(self.cursor)
+    self.cursor = self.cursor - 1
+  end
+end
+
+function Editor:delete()
+  if self.cursor <= #self.content then
+    self.content = self.content:sub(1, self.cursor - 1) .. self.content:sub(self.cursor + 1)
+  end
+end
+
+function Editor:move_cursor_left()
+  self.cursor = math.max(1, self.cursor - 1)
+end
+
+function Editor:move_cursor_right()
+  self.cursor = math.min(#self.content + 1, self.cursor + 1)
+end
+
+function Editor:move_cursor_to_start()
+  self.cursor = 1
+end
+
+function Editor:move_cursor_to_end()
+  self.cursor = #self.content + 1
+end
+
+function Editor:clear()
+  self.content = ""
+  self.cursor = 1
+end
+
+function Editor:set_content(new_content)
+  self.content = new_content
+  self.cursor = #new_content + 1
+end
+
+-- Single global editor for all to use
+editor = Editor.new()
+
+---------------------------------------------------------------------
 -- Loop-related objects ---------------------------------------------
 ---------------------------------------------------------------------
 
