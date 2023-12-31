3bf65a089362ca329e87f3ed2c26175f9e212e88
Brock Wilcox <awwaiid@thelackthereof.org>
2023-12-31 12:53:48 -0500

    Add helper for reading files and evalling code from inside the REPL

--- lib/helper.lua
+++ lib/helper.lua
@@ -58,6 +58,21 @@ function helper.eval(code_string)
   return eval_command()
 end
 
+local open = io.open
+
+function helper.read_file(path)
+    local file = open(path, "rb") -- r read mode and b binary mode
+    if not file then return nil end
+    local content = file:read "*a" -- *a or *all reads the whole file
+    file:close()
+    return content
+end
+
+function helper.evalFile(filename)
+  local fileContent = helper.read_file(filename)
+  return helper.eval(fileContent)
+end
+
 function helper.all_words_have(words, letter, offset)
   for _, word in ipairs(words) do
     if string.sub(word, offset, offset) ~= letter then
